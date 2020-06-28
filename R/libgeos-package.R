5d094d14cf953130f720b29142634637ad3264f9
Dewey Dunnington <dewey@fishandwhistle.net>
2020-06-28 14:49:03 -0300

    fix check errors

--- R/libgeos-package.R
+++ R/libgeos-package.R
@@ -7,3 +7,15 @@
 #' @useDynLib libgeos, .registration = TRUE
 ## usethis namespace: end
 NULL
+
+#' GEOS version information
+#'
+#' @return The version string returned by `GEOSversion();`
+#' @export
+#'
+#' @examples
+#' libgeos_version()
+#'
+libgeos_version <- function() {
+  .Call("libgeos_geos_version")
+}
--- src/libgeos-init.cpp
+++ src/libgeos-init.cpp
@@ -3,8 +3,23 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
+SEXP libgeos_geos_version() {
+  SEXP out = PROTECT(Rf_allocVector(STRSXP, 1));
+  SET_STRING_ELT(out, 0, Rf_mkChar(GEOSversion()));
+  UNPROTECT(1);
+  return out;
+}
+
+static const R_CallMethodDef CallEntries[] = {
+  {"libgeos_geos_version", (DL_FUNC) &libgeos_geos_version, 0},
+  {NULL, NULL, 0}
+};
+
 extern "C" {
   void R_init_libgeos(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+
     /* used by external packages linking to libgeos from C */
     R_RegisterCCallable("libgeos", "GEOSversion", (DL_FUNC) &GEOSversion);
   }
