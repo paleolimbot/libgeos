1fa8fbe34e2f33448aaed59c89e3faafd257eee6
Dewey Dunnington <dewey@fishandwhistle.net>
2020-06-29 14:41:51 -0300

    try compiling sourceRcpp using C++11

--- tests/testthat/helper-source-rcpp.R
+++ tests/testthat/helper-source-rcpp.R
@@ -17,6 +17,12 @@ source_rcpp_libgeos <- function(code, header = '', cache = tempfile(), ..., env 
   if (!dir.exists(cache)) {
     dir.create(cache)
   }
+
+  # compile with C++11 to help with exception handling on Windows i386
+  current_flags <- Sys.getenv("PKG_CXXFLAGS")
+  Sys.setenv("PKG_CXXFLAGS" = "-std=c++11")
+  on.exit(Sys.setenv("PKG_CXXFLAGS" = current_flags))
+
   Rcpp::sourceCpp(code = paste0(header, '\n\n', head, code), env = env, cacheDir = cache, ...)
   cache
 }
