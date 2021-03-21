c5c5185bf230864d4b6fa1bb766ee69e92d56f4a
Dewey Dunnington <dewey@fishandwhistle.net>
2021-03-21 10:48:43 -0300

    simplified cpp-compat layer

--- src/geos_include/libgeos-cpp-compat.h
+++ src/geos_include/libgeos-cpp-compat.h
@@ -2,15 +2,8 @@
 #ifndef LIBGEOS_CPP_COMPAT_H
 #define LIBGEOS_CPP_COMPAT_H
 
-#include <streambuf>
-
-void cpp_compat_printf(const char* fmt, ...);
-void cpp_compat_abort();
-void cpp_compat_exit(int code);
-int cpp_compat_random();
-void cpp_compat_srandom(int seed);
+#include <iostream>
 
 extern std::ostream& cpp_compat_cerr;
-extern std::ostream& cpp_compat_cout;
 
 #endif
